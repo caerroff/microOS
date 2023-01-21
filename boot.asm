b7a9e76be4b34ef8e8d651f06251c20e7aa9fab7
Caerroff <56608891+caerroff@users.noreply.github.com>
2023-01-21 22:16:27 +0100

    first boot

--- boot.asm
+++ boot.asm
@@ -1,6 +1,23 @@
 org 0x7c00 ;to put the origin at the right place
 bits 16 ;starting in 16 bits mode
 
+mov ah, 0x0e
+mov bx, message
+print:
+    mov al, [bx]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp print
+
+
+message:
+    db "Welcome to microOS", 0
+
+end:
+
+jmp $
 
 times 510-($-$$) db 0
 dw 0xAA55
